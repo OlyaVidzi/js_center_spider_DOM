fe45b53362aae3388d8f3ad1e22f869548aa3e29
Olya <olyavidzi97@gmail.com>
2024-07-19 14:29:02 +0300

    make build

---
+++ index.2a129034.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("img.spider");e.style.top="".concat(175,"px"),e.style.left="".concat(175,"px");
+//# sourceMappingURL=index.2a129034.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const spiderEl = document.querySelector("img.spider");
+const wallSize = 400;
+const spiderSize = 50;
+const topPosition = (wallSize - spiderSize) / 2;
+const leftPosition = (wallSize - spiderSize) / 2;
+spiderEl.style.top = `${topPosition}px`;
+spiderEl.style.left = `${leftPosition}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
